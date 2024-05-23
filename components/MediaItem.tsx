7eff5be4d66eeca617a89dba1ed74fa788038b4b
Yury0310 <ostap4uk41@gmail.com>
2024-05-23 16:36:41 +0300

    replace delete button text with trash icon

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -18,8 +18,8 @@ interface MediaItemProps {
 const MediaItem: React.FC<MediaItemProps> = ({
   data,
   onClick,
-  showDelete=false,
-  handleSongsDelete=(id)=>{},
+  showDelete = false,
+  handleSongsDelete = (id) => {},
 }) => {
   const player = usePlayer();
 
@@ -29,15 +29,15 @@ const MediaItem: React.FC<MediaItemProps> = ({
     if (onClick) {
       return onClick(data.id);
     }
-  
+
     return player.setId(data.id);
   };
 
-  return ( 
+  return (
     <div className="flex">
-        <div
-      onClick={handleClick}
-      className="
+      <div
+        onClick={handleClick}
+        className="
         flex 
         items-center 
         gap-x-3 
@@ -47,38 +47,56 @@ const MediaItem: React.FC<MediaItemProps> = ({
         p-2 
         rounded-md
       "
-    >
-      <div 
-        className="
+      >
+        <div
+          className="
           relative 
           rounded-md 
           min-h-[48px] 
           min-w-[48px] 
           overflow-hidden
         "
-      >
-        <Image
-          fill
-          src={imageUrl || "/images/music-placeholder.png"}
-          alt="MediaItem"
-          className="object-cover"
-        />
-      </div>
-      <div className="flex flex-col gap-y-1 overflow-hidden">
-        <p className="text-white truncate">{data.title}</p>
-        <p className="text-neutral-400 text-sm truncate">
-          By {data.author}
-        </p>
-      </div>
-    </div>
-    {
-        showDelete &&
-        <div className="w-fit h-fit" style={{marginLeft:"auto", backgroundColor:"brown", borderRadius:"4px", padding:"4px"}}>
-          <button className="w-fit h-fit" style={{margin:"0px"}} onClick={(id) => handleSongsDelete(id)}>delete</button>
+        >
+          <Image
+            fill
+            src={imageUrl || "/images/music-placeholder.png"}
+            alt="MediaItem"
+            className="object-cover"
+          />
         </div>
-      }
+        <div className="flex flex-col gap-y-1 overflow-hidden">
+          <p className="text-white truncate">{data.title}</p>
+          <p className="text-neutral-400 text-sm truncate">By {data.author}</p>
+        </div>
+      </div>
+      {showDelete && (
+        <div
+          className="w-fit h-fit"
+          style={{
+            marginLeft: "auto",
+            backgroundColor: "brown",
+            borderRadius: "4px",
+            padding: "4px",
+          }}
+        >
+          <button
+            className=""
+            style={{ margin: "0px", height: "57px", width: "25px" }}
+            onClick={(id) => handleSongsDelete(id)}
+          >
+            <img
+              src="https://img.icons8.com/?size=100&id=85194&format=png&color=000000"
+              alt="trash"
+              style={{
+                width: "25px",
+                height: "25px",
+              }}
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
-}
- 
+};
+
 export default MediaItem;
